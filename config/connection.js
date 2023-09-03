1189714bb9681f2e3bfaac9f61d5e8d2a768bd05
Renny <rennymwangi@gmail.com>
2023-09-04 01:20:07 +0930

    Templates for db, connection and api

---
+++ config/connection.js
@@ -0,0 +1,10 @@
+const sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
+  host: "localhost",
+  dialect: "mysql",
+  port: 3306,
+});
+
+module.exports = sequelize;
