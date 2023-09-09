cbff2e4c77cff5fc0e03507877980f46efea6d81
Renny Mwangi <rennymwangi@gmail.com>
2023-09-09 15:40:24 +0930

    Testing login button

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const email = document.querySelector(".email-login").value.trim();
+  const password = document.querySelector(".password-login").value.trim();
 
   if (email && password) {
     const response = await fetch("/api/users/login", {
@@ -24,9 +24,9 @@ const loginFormHandler = async (event) => {
 
 const signupFormHandler = async (event) => {
   event.preventDefault();
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const username = document.querySelector(".username-signup").value.trim();
+  const email = document.querySelector(".email-signup").value.trim();
+  const password = document.querySelector(".password-signup").value.trim();
 
   if (username && email && password) {
     const response = await fetch("/api/users", {
