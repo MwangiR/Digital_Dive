e910db5ee18ebe2b86b0d7213836487189c3c12c
Renny Mwangi <rennymwangi@gmail.com>
2023-09-11 23:35:31 +0930

    Minor changes to functions

--- public/js/userpage.js
+++ public/js/userpage.js
@@ -26,34 +26,8 @@ const postEditHandler = async (event) => {
   }
 };
 
-const newPostHandler = async (event) => {
-  event.preventDefault();
-  console.log("clicked new post");
-
-  const title = document.querySelector(".title-new").value.trim();
-  const content = document.querySelector(".userpost-new").value.trim();
-
-  if (title && content) {
-    const response = await fetch(`/api/posts`, {
-      method: "POST",
-      body: JSON.stringify({
-        title: title,
-        post_content: content,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      document.location.replace("/dashboard");
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 document.addEventListener("submit", function (event) {
   if (event.target.classList.contains("reEditBlog")) {
     postEditHandler(event);
   }
 });
-
-document.querySelector(".newPost").addEventListener("submit", newPostHandler);
