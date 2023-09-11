e4f03cacf850f5b9350342d5f7a0110cfb489f58
Renny Mwangi <rennymwangi@gmail.com>
2023-09-11 20:43:18 +0930

    Testing heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,16 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
-  host: "localhost",
-  dialect: "mysql",
-  port: 3306,
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DBNAME, process.env.DBUSER, process.env.DBPASSWORD, {
+    host: "localhost",
+    dialect: "mysql",
+    port: 3306,
+  });
+}
 
 module.exports = sequelize;
