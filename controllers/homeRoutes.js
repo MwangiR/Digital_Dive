3a28616899cf4e5751365b97ce7ad5553bc9d51d
Renny Mwangi <rennymwangi@gmail.com>
2023-09-11 23:33:04 +0930

    Added `addcomments/id` to add comments

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,6 +65,37 @@ router.get("/dashboard", withAuth, async (req, res) => {
   });
 });
 
+router.get("/addcomments/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!postData) {
+      return res.status(404).json({ message: "No post found with this id!" });
+    }
+    // res.status(200).json(postData);
+    const posts = postData.get({ plain: true });
+
+    res.render("add-comment", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/login", async (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
