5b967fcf4fb38af6a40d726a9d472d7660e74896
Renny Mwangi <rennymwangi@gmail.com>
2023-09-12 21:30:37 +0930

    testing signup

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -65,6 +65,54 @@ router.get("/dashboard", withAuth, async (req, res) => {
   });
 });
 
+router.get("/addcomments/:id", async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: [
+            {
+              model: User,
+              attributes: ["username"],
+            },
+          ],
+        },
+      ],
+    });
+
+    if (!postData) {
+      return res.status(404).json({ message: "No post found with this id!" });
+    }
+    // res.status(200).json(postData);
+    const posts = postData.get({ plain: true });
+
+    res.render("add-comment", {
+      posts,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      //   req.session.username = userData.username;
+      req.session.logged_in = true;
+    });
+
+    console.log("Redirecting to /dashboard");
+    res.redirect("/dashboard");
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
 router.get("/login", async (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
